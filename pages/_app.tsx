58493aaaa0804f2182e24329854308fadf02722c
조기범 <akfmzh1207@naver.com>
2023-07-01 16:21:42 +0900

    async 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Script
+      <script
         src="https://t1.kakaocdn.net/kakao_js_sdk/2.1.0/kakao.min.js"
         integrity="sha384-dpu02ieKC6NUeKFoGMOKz6102CLEWi9+5RQjWSV0ikYSFFd8M3Wp2reIcquJOemx"
         crossOrigin="anonymous"
-      ></Script>
+        async
+      />
     </>
   );
 }
